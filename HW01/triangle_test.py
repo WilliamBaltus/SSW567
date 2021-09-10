d55868c8dffd674e5f77a912fd84de7985f09468
WilliamBaltus <wbaltus21@gmail.com>
2021-09-10 13:05:52 -0400

    added test cases
    all tests passed (7)

--- HW01/triangle_test.py
+++ HW01/triangle_test.py
@@ -4,6 +4,12 @@ from triangle import classify_triangle
 def test_valid_triangle():
     assert classify_triangle(1,1,2) == "Invalid triangle"
 
+def test_valid_triangle2():
+    assert classify_triangle(1,1,0) == "Invalid triangle"
+
+def test_valid_triangle3():
+    assert classify_triangle(1,1,'a') == "Invalid triangle"
+
 def test_equilateral_triangle():
     assert classify_triangle(1,1,1) == "Equilateral triangle"
 
@@ -15,3 +21,4 @@ def test_scalene_triangle():
 
 def test_isosceles_triangle():
     assert classify_triangle(3,3,5) == "Isosceles triangle"
+
